a6a0504e49e2374bc18a3a3424cb76fc0307e755
lucasrossi0 <lucasrossi502@gmail.com>
2024-07-26 11:28:24 +0200

    Update

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: lrossi-u <lrossi-u@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 18:40:04 by lrossi-u          #+#    #+#             */
-/*   Updated: 2024/06/25 19:21:46 by lrossi-u         ###   ########.fr       */
+/*   Updated: 2024/07/26 10:54:08 by lrossi-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,3 @@ void	*ft_memset(void *s, int c, size_t n)
 	}
 	return (s);
 }
-/*
-int    main(void)
-{
-    //char    *s = "lucasrossiemmamccoy";
-    char test[10];
-    char *res = ft_memset(test, '=', sizeof(test));
-    printf("Memset %s\n", res);
-    return (0);
-}
-*/
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,32 +6,26 @@
 /*   By: lrossi-u <lrossi-u@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 12:39:31 by lrossi-u          #+#    #+#             */
-/*   Updated: 2024/07/25 18:44:16 by lrossi-u         ###   ########.fr       */
+/*   Updated: 2024/07/26 11:15:02 by lrossi-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <unistd.h>
 
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else if (n < 0)
 	{
-		ft_putchar_fd('-');
-		ft_putchar_fd('2');
-		ft_putnbr_fd(147483648, fd);
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-n, fd);
 	}
-	if (n < 0)
-	{
-		write(fd, "-", 1);
-		n = -n;
-		ft_putnbr_fd(n, fd);
-	}
-	if (n > 9)
+	else if (n >= 10)
 	{
 		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
+		ft_putchar_fd(n % 10 + '0', fd);
 	}
 	else
-		ft_putchar_fd(n + 48, fd);
+		ft_putchar_fd(n + '0', fd);
 }
